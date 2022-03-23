00895a4ac7dd31287e8c419daa88fb97a66e8806
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-03-23 16:35:07 +0600

    Create simpson_intregret.c

---
+++ simpson_intregret.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int a,b,n;
+double f(double x)
+{
+    return (1/(x));
+}
+double getabs(double x)
+{
+    if(x<0)
+    return (x*-1);
+    else
+    return x;
+}
+int main()
+{
+    double area;
+    printf("Enter lower and Upper limit: ");
+    scanf("%d%d",&a,&b);
+    
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    
+    double h = getabs(((double)b-(double)a)/(double)n);
+     //printf("Area is : %f\n",area);
+    
+    area = f(a)+f(b);
+    //printf("Area is : %f\n",area);
+    
+    double x= (double)a+h;
+    for(int i=1; i<n;i++)
+    {
+       if(i%2==0)
+            area = area+ 2*f(x);
+       else
+            area = area+ 4*f(x);
+         //printf("x=%f  f(x) is=%f Area is : %f\n",x,f(x),area);
+        x=x+h;
+    }
+    area = (area*h)/3;
+    printf("Area is : %f\n",area);
+    
+}
